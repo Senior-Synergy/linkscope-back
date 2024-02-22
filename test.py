c634a92a305ce03444dde2035d9e20de68b570e3
Palida Yingwatchara <poonpalida@gmail.com>
2024-02-22 07:22:22 +0700

    Update

--- test.py
+++ test.py
@@ -6,14 +6,21 @@ import warnings
 import pickle
 import whois
 from urllib.parse import urlparse
+import bz2file as bz2
 warnings.filterwarnings('ignore')
 from app.feature_extraction import FeatureExtraction
 
-with open("data/model.pkl","rb") as f:
-    model = pickle.load(f)
+#with open("data/model.pkl","rb") as f:
+#    model = pickle.load(f)
+
+def decompress_pickle(file):
+    data = bz2.BZ2File(file, "rb")
+    data = pickle.load(data)
+    return data
+model = decompress_pickle("data/model_allfeatures.bz2")
 
 #file.close()
-url = 'https://www.videezy.com'
+url = 'fakespot.com'
 
 #url = request.form["url"]
 def geturlresults(url):
@@ -25,7 +32,7 @@ def geturlresults(url):
     #1 Prediction 
     y_pred = model.predict(extracted_df)[0]                 # 0 means safe, 1 means phish
     y_pro_phishing = model.predict_proba(extracted_df)[0,1] # Probability to be phishing url
-    print("There is", round(y_pro_phishing *100 ,2), "% chance, the url is phishing" ) # Probability to be phishing url
+    print("There is", "%.2f" % (y_pro_phishing *100) , "% chance, the url is phishing" ) # Probability to be phishing url
 
 # Same Results with all urls    
 def getmodelresults(model) :
