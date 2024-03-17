858cec611383cf75bda1a061673199f8939f1a63
Palida Yingwatchara <poonpalida@gmail.com>
2024-03-17 17:39:23 +0700

    Squashed commit of the following:
    commit 9179db11621eee4e9a9a27cdcdc958e2a6bb93ab
    Author: Palida Yingwatchara <poonpalida@gmail.com>
    Date:   Sun Mar 17 17:38:33 2024 +0700
    
        Restructure DB_ver2
    
    commit 698dcdc205c9c40f5323218884ecec40b9888c2a
    Author: Palida Yingwatchara <poonpalida@gmail.com>
    Date:   Sun Mar 17 13:32:02 2024 +0700
    
        Update api scan_all to return scan_id
    
    commit 859ac8cbf2272045149e31785af1cb298a8299b7
    Merge: fa2e395 1996016
    Author: Palida Yingwatchara <poonpalida@gmail.com>
    Date:   Sat Mar 16 22:38:12 2024 +0700
    
        Merge branch 'main' into model
    
    commit fa2e39516ca4da94d926ac1990d02d12a675d904
    Author: Palida Yingwatchara <poonpalida@gmail.com>
    Date:   Sat Mar 16 20:23:04 2024 +0700
    
        Restructure Database
    
    commit edc606dd7824518ffebf8f9e31b0e3d43fc054f1
    Merge: 75d1f36 7437a2d
    Author: Palida Yingwatchara <poonpalida@gmail.com>
    Date:   Sat Mar 16 17:05:45 2024 +0700
    
        Merge branch 'main' into model
    
    commit 75d1f360b64a6411863a67cbab889046f5ac1467
    Author: Palida Yingwatchara <poonpalida@gmail.com>
    Date:   Sat Mar 16 13:41:01 2024 +0700
    
        Restructure Database
    
    commit d320731104a16b4075461cb06f7f3305af07a7ee
    Author: Palida Yingwatchara <poonpalida@gmail.com>
    Date:   Fri Mar 15 23:10:44 2024 +0700
    
        Update from main

--- app/repository/url_crud.py
+++ app/repository/url_crud.py
@@ -6,86 +6,102 @@ from app.constants import feature_names2
 from typing import List
 import random
 #-----------------------------------------CREATE--------------------------------------------------
-# insert to url_submission table
-def create_url_submission(session: Session): 
+# insert to submission table
+def create_submission(session: Session): 
     try:
         #scan_id = random.randint(100000,999999)
-        report_result = models.URLSubmission()
-        session.add(report_result)
+        submission_data = models.Submission()
+        session.add(submission_data)
         session.commit()
-        session.refresh(report_result)
+        session.refresh(submission_data)
     except Exception as e:
         print(f'Error to create_url_submission is {str(e)}')
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail=f"Failed to create a new submission for in the database")
-    return report_result
+                            detail=f"Failed to create a new submission_data")
+    return submission_data
 
-# insert to url_result table
-def create_url_result(url: str, scan_id: int, result: URLresult, session: Session):
+# insert to result table
+def create_result(submission_id :int, url_id : int, submitted_url : str, session: Session):
     try:
-        scan_result = models.URLResult(scan_id=scan_id, 
-                                        url=url,
-                                        final_url=result.get_final_url(),
-                                        phish_prob=result.get_phish_prob(),
-                                        is_phishing=result.get_isPhish())
-        session.add(scan_result)
+        result_data = models.Result(submission_id=submission_id,
+                                     url_id = url_id,
+                                     submitted_url = submitted_url)
+        session.add(result_data)
         session.commit()
-        session.refresh(scan_result)
+        session.refresh(result_data)
     except Exception as e:
         print(f'Error to insert to url_result is {str(e)}')
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail=f"Failed to create a new entry for '{url}' in the database")
-    return scan_result
+                            detail=f"Failed to create a new result_data")
+    return result_data
+
+# insert to url table
+def create_url(feature_id: int, result: URLresult, session: Session):
+    try:
+        url_data = models.Url(feature_id = feature_id, 
+                                       final_url=result.get_final_url(),
+                                        phish_prob=result.get_phish_prob(),
+                                        is_phishing=result.get_isPhish())
+        session.add(url_data)
+        session.commit()
+        session.refresh(url_data)
+    except Exception as e:
+        print(f'Error to insert to url_result is {str(e)}')
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=f"Failed to create a new url_data")
+    return url_data
 
 # insert to url_features table
-def create_url_features(url_id : int, final_url : str, result: URLresult, session: Session):
+def create_feature(result: URLresult, session: Session):
     try:
         features_arr = result.features_arr
         # I will find way to shorten this ....
-        feature_result = models.URLFeatures(url_id = url_id,
-                                                final_url = final_url,
-                                                domainlength = features_arr[0][0], #1
-                                                www = features_arr[0][1], # 2
-                                                subdomain = features_arr[0][2] , # 3
-                                                https = features_arr[0][3] , # 4
-                                                http = features_arr[0][4] , # 5
-                                                short_url = features_arr[0][5] , # 6
-                                                ip = features_arr[0][6] , # 7
-                                                at_count = features_arr[0][7], # 8
-                                                dash_count = features_arr[0][8] , # 9
-                                                equal_count = features_arr[0][9] , # 10
-                                                dot_count = features_arr[0][10] , # 11
-                                                underscore_count = features_arr[0][11] , # 12
-                                                slash_count = features_arr[0][12] , # 13
-                                                digit_count= features_arr[0][13] , # 14
-                                                log_contain = features_arr[0][14] , # 15
-                                                pay_contain = features_arr[0][15], # 16
-                                                web_contain = features_arr[0][16], #17
-                                                cmd_contain = features_arr[0][17], # 18
-                                                account_contain= features_arr[0][18], # 19
-                                                pc_emptylink = features_arr[0][19] , # 20
-                                                pc_extlink = features_arr[0][20], # 21
-                                                pc_requrl= features_arr[0][21] , # 22
-                                                zerolink = features_arr[0][22] , # 23
-                                                ext_favicon = features_arr[0][23], # 24
-                                                submit_to_email = features_arr[0][24] , # 25
-                                                sfh = features_arr[0][25] , # 26
-                                                redirection = features_arr[0][26], # 27
-                                                domainage = features_arr[0][27] , # 28
-                                                domainend = features_arr[0][28] ) # 29 
-        session.add(feature_result)
+        feature_data = models.Feature(domainlength = features_arr[0][0], #1
+                                      www = features_arr[0][1], # 2
+                                      subdomain = features_arr[0][2] , # 3
+                                      https = features_arr[0][3] , # 4
+                                      http = features_arr[0][4] , # 5
+                                      short_url = features_arr[0][5] , # 6
+                                      ip = features_arr[0][6] , # 7
+                                      at_count = features_arr[0][7], # 8
+                                      dash_count = features_arr[0][8] , # 9
+                                      equal_count = features_arr[0][9] , # 10
+                                      dot_count = features_arr[0][10] , # 11
+                                      underscore_count = features_arr[0][11] , # 12
+                                      slash_count = features_arr[0][12] , # 13
+                                      digit_count= features_arr[0][13] , # 14
+                                      log_contain = features_arr[0][14] , # 15
+                                      pay_contain = features_arr[0][15], # 16
+                                      web_contain = features_arr[0][16], #17
+                                      cmd_contain = features_arr[0][17], # 18
+                                      account_contain= features_arr[0][18], # 19
+                                      pc_emptylink = features_arr[0][19] , # 20
+                                      pc_extlink = features_arr[0][20], # 21
+                                      pc_requrl= features_arr[0][21] , # 22
+                                      zerolink = features_arr[0][22] , # 23
+                                      ext_favicon = features_arr[0][23], # 24
+                                      submit_to_email = features_arr[0][24] , # 25
+                                      sfh = features_arr[0][25] , # 26
+                                      redirection = features_arr[0][26], # 27
+                                      domainage = features_arr[0][27] , # 28
+                                      domainend = features_arr[0][28] ) # 29 
+        session.add(feature_data)
         session.commit()
-        session.refresh(feature_result)          
+        session.refresh(feature_data)          
          
     except Exception as e:
-        print(f'Error to insert url_features is {str(e)}')
-        session.rollback()
+        print(f'Error to insert to url_result is {str(e)}')
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=f"Failed to create a new feature_data")
+    return feature_data
+
 
 #-----------------------------------------READ--------------------------------------------------
+
 def search_url(final_url: str, session: Session):
     try:
-        scan_result = session.query(models.URLResult).filter(
-            models.URLResult.final_url == final_url).first()
+        scan_result = session.query(models.Url).filter(
+            models.Url.final_url == final_url).first()
         
     except Exception as e:
         print(str(e))
@@ -93,8 +109,8 @@ def search_url(final_url: str, session: Session):
                             detail=f"Failed to search for '{final_url}' in the database")
         
     return scan_result
-
-def get_url_result_by_url_id(url_id: int, session: Session):
+''' 
+def get_url_data_by_url_id(url_id: int, session: Session):
     try:
         scan_result = session.query(models.URLResult).filter(
             models.URLResult.url_id == url_id).first()
@@ -103,16 +119,16 @@ def get_url_result_by_url_id(url_id: int, session: Session):
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                             detail=f"Failed to access 'scan_id: {url_id}' in the database")
     return scan_result
-
-# Error : get all url result by scan_id(PK of url_submission) 
-def get_url_result_by_scan_id(scan_id: int, session: Session):
+'''
+def get_url_data_by_submission_id(submission_id: int, session: Session):
     try:
-        report_result = session.query(models.URLResult).filter(
-        models.URLResult.scan_id == scan_id).all()
-        print(f'report_result is {report_result}')
+        url_result = session.query(models.Url).join(
+            models.Result, models.Result.url_id == models.Url.url_id).filter(
+            models.Result.submission_id == submission_id
+            )
+        print(f'report_result is {url_result}')
     except Exception as e:
         print(f'error is {str(e)}')
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail=f"Failed to access 'scan_id: {scan_id}' in the database")
-    #print(report_result)
-    return report_result
+                            detail=f"Failed to access 'scan_id: {submission_id}' in the database")
+    return url_result
